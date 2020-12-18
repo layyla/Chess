aa57fdca041623c41b3d3fbbf19be29810ceab9a
mohamed-jemis <62571364+mohamed-jemis@users.noreply.github.com>
2020-12-18 22:59:21 +0200

    Add files via upload

---
+++ ChessGithub/src/chessgithub/Chess OOP project/Piece.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public abstract class Piece {
+
+    private boolean eaten  = false;
+    private boolean white = false ;
+
+    public Piece(boolean white) {
+        this.white = white;
+    }
+
+    public boolean isEaten() {
+        return eaten;
+    }
+
+    public void setEaten(boolean eaten) {
+        this.eaten = eaten;
+    }
+
+    public boolean isWhite() {
+        return white;
+    }
+
+    public void setWhite(boolean white) {
+        this.white = white;
+    }
+
+    public abstract boolean MoveIsValid (Spot current_position , Spot new_position );
+
+}
