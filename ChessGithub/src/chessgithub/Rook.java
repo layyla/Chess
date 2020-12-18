59808433ffce7ca39aa79f1946a1f2a333fac48b
Ziad Alagroudy <62571596+3agroudyJr@users.noreply.github.com>
2020-12-18 23:11:09 +0200

    Update Rook.java

--- ChessGithub/src/chessgithub/Rook.java
+++ ChessGithub/src/chessgithub/Rook.java
@@ -1,87 +1,59 @@
 package chessgithub;
 
+
 public class Rook extends Piece {
-    
-    @Override
-    public boolean MoveIsValid (Spot current_position , Spot new_position) {
-        // checK if new position is valid 
-        if (current_position.getX_position() != new_position.getX_position() && 
-            current_position.getY_position() != new_position.getY_position()   ) {
-            
-            return false;        
-        }
-        // check if new position is not equal to the current_position
-        else if (current_position.getX_position() == new_position.getX_position() && 
-                 current_position.getY_position() == new_position.getY_position()   ) {
-        
-            return false;             
-        }
-        // check if there is no pieces blocking the way from current position to new position
-        else if (current_position.getX_position() != new_position.getX_position()) {
-            int y = current_position.getY_position();
-            if (new_position.getX_position() > current_position.getX_position()) {
-                for (int i = current_position.getX_position() ; i < new_position.getX_position() ; i++) {
-                    // board is class unfinished 
-                    // getSpot method returns spot in the board with coordinates x & y
-                    if (Board.getSpot(i , y).getPiece() != null) {
-                        return false;
-                    }
-                }
-            }
-            else {
-                for (int i = current_position.getX_position() ; i > new_position.getX_position() ; i--) {
-                    // board is class unfinished 
-                    // getSpot method returns spot in the board with coordinates x & y
-                    if (Board.getSpot(i , y).getPiece() != null) {
-                        return false;
-                    }
-                }
-            }
-            if (new_position.getPiece() != null) {
-                if (new_position.getPiece().isWhite() == this.isWhite()) {
-                    return false;
-                }
-            }
-        }
-        else if (current_position.getY_position() != new_position.getY_position()) {
-            int x = current_position.getX_position();
-            if (new_position.getY_position() > current_position.getY_position()) {
-                for (int j = current_position.getY_position() ; j < new_position.getY_position() ; j++) {
-                    // board is class unfinished 
-                    // getSpot method returns spot in the board with coordinates x & y
-                    if (Board.getSpot(x , j).getPiece() != null) {
-                        return false;
-                    }
-                }
-            }
-            else {
-                for (int j = current_position.getY_position() ; j > new_position.getY_position() ; j--) {
-                    // board is class unfinished 
-                    // getSpot method returns spot in the board with coordinates x & y
-                    if (Board.getSpot(x , j).getPiece() != null) {
-                        return false;
-                    }
-                }
-            }
-            if (new_position.getPiece() != null) {
-                if (new_position.getPiece().isWhite() == this.isWhite()) {
-                    return false;
-                }
-            }
-        }
-        // check if new position is outside the board
-        else if (new_position.getX_position() >= 8 || new_position.getX_position() < 0 ||
-                 new_position.getY_position() >= 8 || new_position.getX_position() < 0) {
-            
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-    
-    // subclass constructor
-    public Rook (boolean white) {
+
+    public Rook(boolean white) {
         super(white);
+    }
+
+    @Override
+    public boolean MoveIsValid(Piece[][] board, Spot current_position, Spot new_position) {
+       // TODO Auto-generated method stub
+		
+		//System.out.println("currentRow: " + currentRow + " currentCol: " + currentCol + " newRow: " + newRow + " newCol: " + newCol);
+		
+		if(current_position.getX_position() != new_position.getX_position() && current_position.getY_position() != new_position.getY_position()){
+			//Did not move along one rank/file
+			return false;
+		}
+		
+		//First I will assumed the Rook is moving along the rows.
+		int offset;
+		
+		if(current_position.getX_position() != new_position.getX_position()){
+			if(current_position.getX_position() < new_position.getX_position()){
+				offset = 1;
+			}else{
+				offset = -1;
+			}
+			
+			for(int x = current_position.getX_position() + offset; x != new_position.getX_position(); x += offset){
+				//Go from currentRow to newRow, and check every space
+				if(board[x][current_position.getY_position()] != null){
+					//System.out.println("1 " + x);
+					return false;
+				}
+			}
+		}
+	
+		//Now do the same for columns
+		if(current_position.getY_position() != new_position.getY_position()){
+			if(current_position.getY_position() < new_position.getY_position()){
+				offset = 1;
+			}else{
+				offset = -1;
+			}
+			
+			for(int x = current_position.getY_position() + offset; x != new_position.getY_position(); x += offset){
+				//Go from currentCol to newCol, and check every space
+				if(board[current_position.getX_position()][x] != null){
+					//System.out.println("2");
+					return false;
+				}
+			}
+		}
+		
+		return true; 
     }
 }
